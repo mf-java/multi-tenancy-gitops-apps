d57409e4b597fe34c0f730c19306d70d9d85f2cb
mark.alfy@ibm.com <mark.alfy@ibm.com>
2022-08-15 12:57:15 -0400

    fixxxxxxxxx

--- sample-java-app/environments/dev/mq-qm01/certificates/dev-mq-server-certificate.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-CLUSTER_DOMAIN=$(oc get dns cluster -o jsonpath='{ .spec.baseDomain }')
-
-# Create Kubernetes Secret yaml
-( echo "cat <<EOF" ; cat dev-sample-java-app-server-certificate.yaml_template ; echo EOF ) | \
-CLUSTER_DOMAIN=${CLUSTER_DOMAIN} \
-sh > dev-sample-java-app-server-certificate.yaml
